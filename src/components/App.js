1f82cadfd5c6b38598c7c5bd2bdba0505a1bc387
younghwani <younghwankim624@gmail.com>
2021-03-17 01:18:03 +0900

    Cleaning

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,19 @@ import { authService } from "fbase";
 
 function App() {
 	const [init, setInit] = useState(false);
-	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
 	const [userObj, setUserObj] = useState(null);
 	useEffect(() => {
 		authService.onAuthStateChanged((user) => {
 			if (user) {
-				setIsLoggedIn(true);
 				//방법1. userObj에서 사용할 요소만 선택함.(object가 무거우면 react가 re-랜더링 요소를 찾는 것 힘들어함.)
-				/*
 				setUserObj({
 					displayName: user.displayName,
 					uid: user.uid,
 					updateProfile: (args) => user.updateProfile(args),
 				});
-				*/
-				//방법2. 깊은 복사, 얕은 복사 이용
-				setUserObj(user);
+				//방법2. 깊은 복사, 얕은 복사 이용 (profile을 2번 이상 연속 update 시 error)
+				//setUserObj(user);
 			} else {
-				setIsLoggedIn(false);
 				setUserObj(null);
 			}
 			setInit(true);
@@ -29,23 +24,21 @@ function App() {
 	}, []);
 	const refreshUser = () => {
 		//방법1
-		/*
 		const user = authService.currentUser;
 		setUserObj({
 			displayName: user.displayName,
 			uid: user.uid,
 			updateProfile: (args) => user.updateProfile(args),
 		});
-		*/
 		//방법2
-		setUserObj({ ...authService.currentUser });
+		//setUserObj({ ...authService.currentUser });
 	};
 	return (
 		<>
 			{init ? (
 				<AppRouter
 					refreshUser={refreshUser}
-					isLoggedIn={isLoggedIn}
+					isLoggedIn={Boolean(userObj)}
 					userObj={userObj}
 				/>
 			) : (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import Rweet from "components/Rweet";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "fbase";
+import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
+import RweetFactory from "components/RweetFactory";
 
 const Home = ({ userObj }) => {
-	const [rweet, setRweet] = useState("");
 	const [rweets, setRweets] = useState([]);
-	const [attachment, setAttachment] = useState("");
 	useEffect(() => {
 		dbService.collection("rweets").onSnapshot((snapshot) => {
 			const rweetArray = snapshot.docs.map((doc) => ({
@@ -16,69 +14,9 @@ const Home = ({ userObj }) => {
 			setRweets(rweetArray);
 		});
 	}, []);
-	const onSubmit = async (event) => {
-		event.preventDefault();
-		let attachmentUrl = "";
-		if (attachment !== "") {
-			const attachmentRef = storageService
-				.ref()
-				.child(`${userObj.uid}/${uuidv4()}`);
-			const response = await attachmentRef.putString(
-				attachment,
-				"data_url"
-			);
-			attachmentUrl = await response.ref.getDownloadURL();
-		}
-		const rweetObj = {
-			text: rweet,
-			createdAt: Date.now(),
-			creatorId: userObj.uid,
-			attachmentUrl,
-		};
-		await dbService.collection("rweets").add(rweetObj);
-		setRweet("");
-		setAttachment("");
-	};
-	const onChange = (event) => {
-		const {
-			target: { value },
-		} = event;
-		setRweet(value);
-	};
-	const onFileChange = (event) => {
-		const {
-			target: { files },
-		} = event;
-		const theFile = files[0];
-		const reader = new FileReader();
-		reader.onloadend = (finishedEvent) => {
-			const {
-				currentTarget: { result },
-			} = finishedEvent;
-			setAttachment(result);
-		};
-		reader.readAsDataURL(theFile);
-	};
-	const onClearAttachment = () => setAttachment(null);
 	return (
 		<div>
-			<form onSubmit={onSubmit}>
-				<input
-					value={rweet}
-					onChange={onChange}
-					type="text"
-					placeholder="What's on yout mind?"
-					maxLength={120}
-				/>
-				<input type="file" accept="image/*" onChange={onFileChange} />
-				<input type="submit" value="Rweet" />
-				{attachment && (
-					<div>
-						<img src={attachment} width="50px" height="50px" />
-						<button onClick={onClearAttachment}>Clear</button>
-					</div>
-				)}
-			</form>
+			<RweetFactory userObj={userObj} />
 			<div>
 				{rweets.map((rweet) => (
 					<Rweet
