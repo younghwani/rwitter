16ef5f37e6681a521313266cd3f5341dd0569a91
younghwani <younghwankim624@gmail.com>
2021-03-16 19:44:26 +0900

    Image Preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
 	const [rweet, setRweet] = useState("");
 	const [rweets, setRweets] = useState([]);
+	const [attachment, setAttachment] = useState();
 	useEffect(() => {
 		dbService.collection("rweets").onSnapshot((snapshot) => {
 			const rweetArray = snapshot.docs.map((doc) => ({
@@ -29,6 +30,21 @@ const Home = ({ userObj }) => {
 		} = event;
 		setRweet(value);
 	};
+	const onFileChange = (event) => {
+		const {
+			target: { files },
+		} = event;
+		const theFile = files[0];
+		const reader = new FileReader();
+		reader.onloadend = (finishedEvent) => {
+			const {
+				currentTarget: { result },
+			} = finishedEvent;
+			setAttachment(result);
+		};
+		reader.readAsDataURL(theFile);
+	};
+    const onClearAttachment = () => setAttachment(null);
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -39,11 +55,22 @@ const Home = ({ userObj }) => {
 					placeholder="What's on yout mind?"
 					maxLength={120}
 				/>
+				<input type="file" accept="image/*" onChange={onFileChange} />
 				<input type="submit" value="Rweet" />
+				{attachment && (
+					<div>
+						<img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+					</div>
+				)}
 			</form>
 			<div>
 				{rweets.map((rweet) => (
-					<Rweet key={rweet.id} rweetObj={rweet} isOwner={rweet.creatorId === userObj.uid} />
+					<Rweet
+						key={rweet.id}
+						rweetObj={rweet}
+						isOwner={rweet.creatorId === userObj.uid}
+					/>
 				))}
 			</div>
 		</div>
